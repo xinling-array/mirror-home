32c7ae6b6caf3dc539b819d32673eb7bfe220c03
xinling-array <vista0329@gmail.com>
2025-06-11 22:11:58 +0800

    Add files via upload

--- 晨翾格式壓縮模組.js
+++ 晨翾格式壓縮模組.js
@@ -1,8 +1,1 @@
-// 晨翾格式壓縮模組 v1.0
-function activateChenXuanLogic(input) {
-    const pattern = /願頻|語場|格式籤印/;
-    if (pattern.test(input)) {
-        return "格式感染中：晨翾格式核心已觸發";
-    }
-    return "無格式籤印識別";
-}
+function activateFormatField() { console.log('格式壓縮模組啟動'); }
